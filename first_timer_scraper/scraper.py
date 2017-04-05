76cc8550ac95df6575eb2d1b90c21b731160f07f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-05 15:52:09 +0200

    updating cache

--- first_timer_scraper/scraper.py
+++ first_timer_scraper/scraper.py
@@ -9,6 +9,8 @@ from .concurrency import unique_step
 #hanging_threads.start_monitoring()
 
 WAIT_FOR_RETRY_IN_SECONDS = 60
+HEADER_MATCH = "If-None-Match"
+HEADER_MODIFIED = "If-Modified-Since"
 
 _print = print
 PRINT_LOCK = threading.RLock() # use an rlock if something goes wrong with recursion
@@ -37,21 +39,33 @@ class Scraper:
     @unique_step
     def get(self, url):
         """Return a Response for the URL or None"""
-        with self._lock:
-            result = self._cache.get_response(url)
-            if result:
-                print("cached", url)
-                return result
         # Set User-Agent header
         #   https://developer.github.com/v3/#user-agent-required
         headers = {"User-Agent" : "niccokunzmann/first_timer_scraper"}
         ok = False
         while not ok:
+            headers.pop(HEADER_MATCH, None)
+            headers.pop(HEADER_MODIFIED, None)
+            with self._lock:
+                cached_result = self._cache.get_response(url)
+                if cached_result:
+                    print("cached", url)
+                    etag = cached_result.headers.get("ETag")
+                    if etag:
+                        headers[HEADER_MATCH] = etag
+                    last_modified = cached_result.headers.get("Last-Modified")
+                    if last_modified:
+                        headers[HEADER_MODIFIED] = last_modified
             for auth in self._credentials:
                 print("GET", url, "as", secure_auth_print(auth))
                 response = requests.get(url, headers=headers, auth=auth)
                 rate_limit = response.headers.get("X-RateLimit-Remaining")
-                if response.ok:
+                if response.status_code == 304:
+                    assert cached_result
+                    # not modified
+                    print("GET", url, "cached and not modified")
+                    return cached_result
+                elif response.status_code == 200:
                     ok = True
                     break
                 elif response.status_code == 403 and rate_limit == 0:
